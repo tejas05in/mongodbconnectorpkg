e9fc5ccf3e5a89d84ccb03a09ef5a734dc700825
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2023-12-30 01:56:09 +0530

    code and version updated

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "Mongo-Connect"
+PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "tejas05in"
 AUTHOR_EMAIL = "tejas05in@yahoo.co.in"
 
